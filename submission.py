b6029e0b79ec427f553f8bcce0c58cd32128bb19
Siddhant Kuwar <siddhankuwar116@gmail.com>
2026-04-01 19:49:11 -0700

    second iteration

--- submission.py
+++ submission.py
@@ -15,24 +15,37 @@ def custom_kernel(data: input_t) -> output_t:
     gemm_a4w4 with bpreshuffle=True.
     """
     import aiter
-    from aiter import QuantType, dtypes
+    import torch
+    from aiter import dtypes
     from aiter.ops.triton.quant import dynamic_mxfp4_quant
     from aiter.utility.fp4_utils import e8m0_shuffle
 
-    def _quant_mxfp4(x, shuffle=True):
+    def _quant_mxfp4(x):
         x_fp4, bs_e8m0 = dynamic_mxfp4_quant(x)
-        if shuffle:
-            bs_e8m0 = e8m0_shuffle(bs_e8m0)
+        bs_e8m0 = e8m0_shuffle(bs_e8m0)
         return x_fp4.view(dtypes.fp4x2), bs_e8m0.view(dtypes.fp8_e8m0)
 
-    A, B, B_q, B_shuffle, B_scale_sh = data
+    A, _, _, B_shuffle, B_scale_sh = data
     A = A.contiguous()
-    B = B.contiguous()
-    m, k = A.shape
-    n, _ = B.shape
+    m, _ = A.shape
+    n = B_shuffle.shape[0]
 
-    A_q, A_scale_sh = _quant_mxfp4(A, shuffle=True)
-    out_gemm = aiter.gemm_a4w4(
+    A_q, A_scale_sh = _quant_mxfp4(A)
+    if m < 32:
+        out = torch.empty(((m + 31) // 32 * 32, n), dtype=dtypes.bf16, device=A.device)
+        out_gemm = aiter.gemm_a4w4_asm(
+            A_q,
+            B_shuffle,
+            A_scale_sh,
+            B_scale_sh,
+            out,
+            "_ZN5aiter41f4gemm_bf16_per1x32Fp4_BpreShuffle_32x128E",
+            bpreshuffle=True,
+            log2_k_split=0,
+        )
+        return out_gemm[:m]
+
+    return aiter.gemm_a4w4(
         A_q,
         B_shuffle,
         A_scale_sh,
@@ -40,4 +53,3 @@ def custom_kernel(data: input_t) -> output_t:
         dtype=dtypes.bf16,
         bpreshuffle=True,
     )
-    return out_gemm
